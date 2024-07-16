bcc75ec95a6df3d2a85dd44d37943363edc50d90
cy3erdr4g0n <imrg10400@gmail.com>
2024-07-16 18:33:10 +0100

    changes

--- README.md
+++ README.md
@@ -39,10 +39,7 @@ git --version
 
 #### To Fork the repo click on the fork button at the top right of the page. Once the repo is forked open your terminal and perform the following commands
 
-```
-git clone hhttps://github.com/<YOUR GITHUB USERNAME>/attaullah.git
-cd attaullah
-```
+
 ### Install packages from the root directory
 ```
 npm install
--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
     description1: "I’m a software developer, cyber security Enthusiast,SecOps and Multi-Cloud Enthusiast, nodejs and Python Cody. I’m currently doing my BT in cyber security, and a proffesional degress in cyber security from SQI College of ICT while working on other projects.",
-    description2: "I am always been passionate about Cybersecurity, Multi-Cloud, and am also a passionate researcher. When I’m not developing and building things, you can find me on online learning, writing an article, daydreaming action-packed dreams where I am the MC, or on my laptop. But most of my days are spent working on projects, coding on Nodejs or Python, Studying or mediating.",
+    description2: "I am always been passionate about Cybersecurity, Multi-Cloud, and am also a passionate researcher. When I’m not developing and building things, you can find me on online learning. But most of my days are spent working on projects, coding on Nodejs or Python, Studying or mediating.",
     image: 2
 }
